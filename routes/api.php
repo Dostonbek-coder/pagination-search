cdbb59882ad27fa78c6b2f9edb8ec4e55e59c6d4
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-05-28 11:02:22 +0500

    All routers have been configured.

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('users', [UserController::class, 'index']);
+
+Route::apiResource('/posts', PostController::class);
